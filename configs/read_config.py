63805659139630973d1182a435672d6e96e1dde9
aduotabilwilfred <otabilwilfred@gmail.com>
2026-06-20 15:44:57 +0000

    pre-commit hook

--- configs/read_config.py
+++ configs/read_config.py
@@ -1,10 +1,12 @@
 import os
+
 import yaml
 
 config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
 
 with open(config_path, "r") as f:
     content = yaml.safe_load(f)
+
 
 def get_config(name):
     return content[name]
