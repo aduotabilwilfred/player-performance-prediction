caf936b21d1f81b27a12f635769cdaa3455c2aab
aduotabilwilfred <otabilwilfred@gmail.com>
2026-06-22 12:50:30 +0000

    untrack unecessary files

--- .ipynb_checkpoints/read_config-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open('./config.yaml', 'r') as f:
-    content = yaml.safe_load(f)
-
-
-print("Model:", content['model_name'])
-print("Learning rate:", content['learning_rate'])
-print("Epochs:", content['epochs'])
