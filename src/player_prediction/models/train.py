3c1e310af6e852fb0fa66cc546470f74526773b8
aduotabilwilfred <otabilwilfred@gmail.com>
2026-06-24 23:55:14 +0000

    dvc parameterize

--- src/player_prediction/models/train.py
+++ src/player_prediction/models/train.py
@@ -1,11 +1,13 @@
 import os
+
 import yaml
+
 
 def load_parameters():
     params_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../params.yaml"))
-    with open(params_path, 'r') as f:
+    with open(params_path, "r") as f:
         config = yaml.safe_load(f)
-    return config['train']
+    return config["train"]
 
 
 def main():
@@ -14,5 +16,5 @@ def main():
     print(f"Training for {params['epochs']} epochs with learning rate {params['learning_rate']}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
