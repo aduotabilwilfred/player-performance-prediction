d76295606a1fb556b3813251f2baf9018e9a752b
aduotabilwilfred <otabilwilfred@gmail.com>
2026-07-04 21:28:30 +0000

    xgboost defined

--- src/player_prediction/models/train.py
+++ src/player_prediction/models/train.py
@@ -1,20 +1,48 @@
 import os
-
+import json
 import yaml
+import pickle
+import pandas as pd
+from xgboost import XGBRegressor
 
 
 def load_parameters():
     params_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../params.yaml"))
     with open(params_path, "r") as f:
         config = yaml.safe_load(f)
-    return config["train"]
+    return config["xgboost_train"]
 
 
 def main():
     params = load_parameters()
     print(params)
-    print(f"Training for {params['epochs']} epochs with learning rate {params['learning_rate']}")
+    
+    model = XGBRegressor(
+        n_estimators=params["n_estimators"],
+        max_depth=params["max_depth"],
+        learning_rate=params["learning_rate"],
+        random_state=params["random_state"],
+        n_jobs=params["n_jobs"]
+    )
+
+    model.fit(X_train, y_train)
+    val_r2 = model.score(X_val, y_val)
+    test_r2 = model.score(X_test, y_test)
+    metrics = {
+        "test_r2": test_r2,
+        "val_r2": val_r2
+    }
+    with open("metrics.json", "w") as f:
+        json.dump(metrics, f)
+
+    model_path = os.path.join(os.path.dirname(__file__), "../../../models/model.pkl")
+    with open(os.path.abspath(model_path), "wb") as f:
+        pickle.dump(model, f)
 
 
 if __name__ == "__main__":
+    # load data
+    train_df = pd.read_csv("data/processed/train.csv")
+    val_df = pd.read_csv("data/processed/val.csv")
+    test_df = pd.read_csv("data/processed/test.csv")
     main()
